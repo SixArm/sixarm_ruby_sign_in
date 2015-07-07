431ffafcec37105cf93547e689038e4ec62f2bff
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-07-07 11:36:55 -0600

    Refactor single-quote to double-quote

--- test/sixarm_ruby_sign_in_test.rb
+++ test/sixarm_ruby_sign_in_test.rb
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-require 'minitest/autorun'
-require 'simplecov'
+require "minitest/autorun"
+require "simplecov"
 SimpleCov.start
-require 'sixarm_ruby_sign_in'
+require "sixarm_ruby_sign_in"
 
 class SignInTest < Test::Unit::TestCase
   
